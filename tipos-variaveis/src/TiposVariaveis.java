de91370e71864b1f357c4f0fd58fdd00c53ff828
raphael690 <raphael.tecnologiainformacaorj@gmail.com>
2023-01-28 20:35:40 -0300

    versao2.1

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,14 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
+/* 
         byte idade = 123;
         short ano = 2023;
         int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
         long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
         float pi = 3.14F;
         double salario = 1275.33;
-
+*/
 
         // Tipos E Variaveis.java
 /* 
